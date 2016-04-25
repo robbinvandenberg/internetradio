cab1086b830f63bf0603e59579040507738e8244
niknak20 <nick__v@hotmail.com>
2016-04-25 12:53:35 +0200

    mouse fix and backbutton

--- src/SettingsController.java
+++ src/SettingsController.java
@@ -102,10 +102,10 @@ public class SettingsController extends JFrame implements ActionListener, Change
 		// Transparent 16 x 16 pixel cursor image.
 		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
 		// Create a new blank cursor.
-		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
-		    cursorImg, new Point(0, 0), "blank cursor");
+		//Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+		  //  cursorImg, new Point(0, 0), "blank cursor");
 		// Set the blank cursor to the JFrame.
-		getContentPane().setCursor(blankCursor);
+		//getContentPane().setCursor(blankCursor);
 		
 		getContentPane().setBackground(new Color(192, 192, 192));
 		setResizable(false);
