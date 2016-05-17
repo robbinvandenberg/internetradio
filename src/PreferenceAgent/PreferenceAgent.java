702e6f8fb4de6ed6d8814a58000b57a04d4a507d
niknak20 <nick__v@hotmail.com>
2016-05-17 12:17:31 +0200

    added preference agent and handlers

---
+++ src/PreferenceAgent/PreferenceAgent.java
@@ -0,0 +1,34 @@
+package PreferenceAgent;
+
+import RadioPlayer.MainMenu;
+import RadioPlayer.MusicController;
+import jade.core.Agent;
+
+/**
+ * RadioPlayer.PreferenceAgent.java
+ * The RadioPlayer.PreferenceAgent class
+ *
+ * This class is used to sort the favorite menu. The favorites are sorted from most played radiostations descending.
+ */
+public class PreferenceAgent extends Agent {
+    private static final long serialVersionUID = 1L;
+    private MainMenu mainMenu;
+    private MusicController musicController;
+    private StationPreferenceHandler stationPreferenceHandler;
+    private VolumePreferenceHandler volumePreferenceHandler;
+
+    /**
+     * The setup function.
+     *
+     * This function is to set the instance of RadioPlayer.MainMenu and set the time interval of the tickerbehaviour.
+     */
+    public void setup() {
+        stationPreferenceHandler = new StationPreferenceHandler();
+        volumePreferenceHandler = new VolumePreferenceHandler();
+
+        musicController = new MusicController();
+        musicController.setOnRadioStationChangedListener(stationPreferenceHandler);
+        mainMenu = new MainMenu(musicController);
+        mainMenu.setVisible(true);
+    }
+}
